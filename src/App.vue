41d03cc8c88b2902aae07f29c8b1f94099f4007d
Hisashi <hisasi622@gmail.com>
2022-06-09 08:16:15 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+
+const number = ref(0);
+
+const add = () => {
+  number.value++;
+};
+</script>
+
+<template>
+  <div>
+    <input type="text" size="30" v-model="number" />
+    <button @click="add()">ボタン</button>
+  </div>
+</template>
